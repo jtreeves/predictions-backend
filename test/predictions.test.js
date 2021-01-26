ef3593acf10d1bb10da9450bf30bdc9dbd00c8b1
jtreeves <jackson.t.reeves@gmail.com>
2021-01-25 23:48:13 -0500

    [test] Delete erroneous test

--- test/predictions.test.js
+++ test/predictions.test.js
@@ -39,48 +39,6 @@ describe('PREDICTIONS: POST route for /:id', () => {
     })
 })
 
-// Test GET route for predictions/:id
-describe('PREDICTIONS: GET route for /:id', () => {
-    it('displays data for a specific prediction', async () => {
-        const loggedUser = await request(app)
-            .post('/users/login')
-            .set('Content-Type', 'application/x-www-form-urlencoded')
-            .send({
-                email: 'victoria@email.com',
-                password: 'victoria1234'
-            })
-        console.log(`LOGGEDUSER: ${loggedUser}`)
-        console.log(`LOGGEDUSER.BODY.TOKEN: ${loggedUser.body.token}`)
-        const foundUser = await db.User.findOne({
-            email: 'victoria@email.com'
-        })
-        console.log(`FOUNDUSER: ${foundUser}`)
-        const foundPrediction = await db.Prediction.findOne({
-            user: foundUser._id
-        })
-        console.log(`FOUNDPREDICTION: ${foundPrediction}`)
-        console.log(`FOUNDPREDICTION._ID: ${foundPrediction._id}`)
-        console.log(`FOUNDPREDICTION KEYS: ${Object.keys(foundPrediction)}`)
-        const getPrediction = await request(app)
-            .get(`/predictions/${foundUser._id}`)
-            .set('Authorization', loggedUser.body.token)
-        let matchPredictions
-        console.log(`GETPREDICTION: ${getPrediction}`)
-        console.log(`GETPREDICTION.BODY: ${getPrediction.body}`)
-        console.log(`GETPREDICTION.BODY.PREDICTION: ${getPrediction.body.prediction}`)
-        console.log(`GETPREDICTION.BODY.PREDICTION._ID: ${getPrediction.body.prediction._id}`)
-        console.log(`GETPREDICTION KEYS: ${Object.keys(getPrediction)}`)
-        console.log(`GETPREDICTION.BODY KEYS: ${Object.keys(getPrediction.body)}`)
-        console.log(`GETPREDICTION.BODY.PREDICTION KEYS: ${Object.keys(getPrediction.body.prediction)}`)
-        if (getPrediction.body.prediction._id == foundPrediction._id) {
-            matchPredictions = true
-        } else {
-            matchPredictions = false
-        }
-        expect(matchPredictions).to.equal(true)
-    })
-})
-
 // Test GET route for predictions/all/:id
 describe('PREDICTIONS: GET route for /all/:id', () => {
     it('returns all predictions associated with a specific user', async () => {
