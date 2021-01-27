9208f58d6f3878db098d443ce9c00bf5c70d6caa
jtreeves <jackson.t.reeves@gmail.com>
2021-01-27 03:01:01 -0500

    [api] Move individuation function into route

--- controllers/api.js
+++ controllers/api.js
@@ -4,11 +4,11 @@ const express = require('express')
 const axios = require('axios')
 const passport = require('passport')
 
-// Import internal middleware
-const individuation = require('../middleware/individuation')
-
 // Import internal models
 const db = require('../models')
+
+// Import internal middleware
+const generation = require('../middleware/generation')
 
 // Define constants
 const key = process.env.REGRESSIONZ_API_KEY
@@ -19,7 +19,7 @@ const router = express.Router()
 
 // Create POST route for api/ (Private)
 router.post('/', passport.authenticate('jwt', {session: false}), async (req, res) => {
-    const source = individuation()
+    const source = generation()
     console.log(`SOURCE: ${source}`)
     const submission = {
         'title': req.body.title,
@@ -30,22 +30,67 @@ router.post('/', passport.authenticate('jwt', {session: false}), async (req, res
     console.log(`SUBMISSION: ${submission}`)
     console.log(`SUBMISSION KEYS: ${Object.keys(submission)}`)
     try {
-        await axios.post(
-            regressionz + '?key=' + key + '&source=' + source,
-            submission
-        )
-        const receivedRegressions = await axios.get(
-            regressionz + '?key=' + key + '&source=' + source
-        )
-        console.log(`RECEIVEDREGRESSIONS: ${receivedRegressions}`)
-        console.log(`RECEIVEDREGRESSIONS KEYS: ${Object.keys(receivedRegressions)}`)
-        res.status(200).json({regressions: receivedRegressions})
+        const foundPrediction = await db.Prediction.findOne({
+            source: source
+        })
+        console.log(`FOUNDPREDICTION: ${foundPrediction}`)
+        console.log(`FOUNDPREDICTION KEYS: ${Object.keys(foundPrediction)}`)
+        console.log(`FOUNDPREDICTION._ID: ${foundPrediction._id}`)
+        if (foundPrediction._id) {
+            return false
+        } else {
+            try {
+                await axios.post(
+                    regressionz + '?key=' + key + '&source=' + source,
+                    submission
+                )
+                const receivedRegressions = await axios.get(
+                    regressionz + '?key=' + key + '&source=' + source
+                )
+                console.log(`RECEIVEDREGRESSIONS: ${receivedRegressions}`)
+                console.log(`RECEIVEDREGRESSIONS KEYS: ${Object.keys(receivedRegressions)}`)
+                res.status(200).json({regressions: receivedRegressions})
+            } catch (error) {
+                console.log(`ERROR: ${error}`)
+                console.log(`ERROR KEYS: ${Object.keys(error)}`)
+                res.status(400).json({msg: error})
+            }
+        }
     } catch (error) {
         console.log(`ERROR: ${error}`)
-        console.log(`ERROR KEYS: ${Object.keys(error)}`)
         res.status(400).json({msg: error})
     }
 })
+
+// // Create POST route for api/ (Private)
+// router.post('/', passport.authenticate('jwt', {session: false}), async (req, res) => {
+//     const source = individuation()
+//     console.log(`SOURCE: ${source}`)
+//     const submission = {
+//         'title': req.body.title,
+//         'independent': req.body.independent,
+//         'dependent': req.body.dependent,
+//         'data_set': req.body.dataSet
+//     }
+//     console.log(`SUBMISSION: ${submission}`)
+//     console.log(`SUBMISSION KEYS: ${Object.keys(submission)}`)
+//     try {
+//         await axios.post(
+//             regressionz + '?key=' + key + '&source=' + source,
+//             submission
+//         )
+//         const receivedRegressions = await axios.get(
+//             regressionz + '?key=' + key + '&source=' + source
+//         )
+//         console.log(`RECEIVEDREGRESSIONS: ${receivedRegressions}`)
+//         console.log(`RECEIVEDREGRESSIONS KEYS: ${Object.keys(receivedRegressions)}`)
+//         res.status(200).json({regressions: receivedRegressions})
+//     } catch (error) {
+//         console.log(`ERROR: ${error}`)
+//         console.log(`ERROR KEYS: ${Object.keys(error)}`)
+//         res.status(400).json({msg: error})
+//     }
+// })
 
 // Export router
 module.exports = router
