076c6206db2f121251dffdbf06364434a242f18c
jtreeves <jackson.t.reeves@gmail.com>
2021-01-27 05:37:03 -0500

    [api] Delete unnecessary logs

--- controllers/api.js
+++ controllers/api.js
@@ -20,25 +20,16 @@ const router = express.Router()
 // Create POST route for api/ (Private)
 router.post('/', passport.authenticate('jwt', {session: false}), async (req, res) => {
     const source = generation()
-    console.log(`SOURCE: ${source}`)
     const submission = {
         'title': req.body.title,
         'independent': req.body.independent,
         'dependent': req.body.dependent,
         'data_set': JSON.parse(req.body.dataSet)
     }
-    console.log(`SUBMISSION: ${submission}`)
-    console.log(`SUBMISSION.TITLE: ${submission.title}`)
-    console.log(`SUBMISSION.DATA_SET: ${submission.data_set}`)
-    console.log(`SUBMISSION KEYS: ${Object.keys(submission)}`)
     const foundPrediction = await db.Prediction.findOne({
         source: source
     })
-    console.log(`FOUNDPREDICTION: ${foundPrediction}`)
-    // console.log(`FOUNDPREDICTION KEYS: ${Object.keys(foundPrediction)}`)
-    // console.log(`FOUNDPREDICTION._ID: ${foundPrediction._id}`)
     if (foundPrediction == null) {
-        console.log('INSIDE IF BLOCK')
         await axios.post(
             regressionz + '?key=' + key + '&source=' + source,
             submission
@@ -46,10 +37,6 @@ router.post('/', passport.authenticate('jwt', {session: false}), async (req, res
         const receivedRegressions = await axios.get(
             regressionz + '?key=' + key + '&source=' + source
         )
-        console.log(`RECEIVEDREGRESSIONS: ${receivedRegressions}`)
-        console.log(`RECEIVEDREGRESSIONS.DATA: ${receivedRegressions.data}`)
-        console.log(`RECEIVEDREGRESSIONS KEYS: ${Object.keys(receivedRegressions)}`)
-        console.log(`RECEIVEDREGRESSIONS.DATA KEYS: ${Object.keys(receivedRegressions.data)}`)
         res.status(200).json({regressions: receivedRegressions.data})
     } else {
         return false
